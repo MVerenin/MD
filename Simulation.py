96da5ee0eab957dda0962749478e60a4bd5107af
Maxim Verenin <maxverenin@mail.ru>
2021-01-07 13:58:48 +0300

    Fourth commit

--- Simulation.py
+++ Simulation.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 class Particle:
     def __init__(self, coord, velocity, mass, L): #задаются координаты частицы, ее скорость, масса и ограничение на перемещение
+        self.initial_coord = np.array(coord)
         self.coord = np.array(coord)
         self.velocity = np.array(velocity)
         self.mass = mass
@@ -19,7 +20,7 @@ class Particle:
         return delta
     def Force(self, other): #сила взаимодействия между двумя частицами
         r = self.Distance(other)
-        return 8*r**(-7)*(2*r**(-6)-1) #используем потенциал Леннарда-Джонса
+        return -24*r**(-7)*(2*r**(-6)-1) #используем потенциал Леннарда-Джонса
     def VectorForce(self, other): #координаты вектора силы взаимодействия между двумя частицами
         return self.Force(other)/self.Distance(other)*self.VectorDistance(other)
     def Acceleration(self, other): #ускорение, приобретаемое частицей под действием такой силы
@@ -29,45 +30,51 @@ class Particle:
     def Move(self, delta_x): #перемещение частицы на заданный вектор
         new_coord = self.coord + np.array(delta_x)
         for e in range(3):
-            if new_coord[e] > 10:
+            if new_coord[e] > 6:
                 new_coord[e] -= self.L
             if new_coord[e] < 0:
                 new_coord[e] += self.L
         self.coord = new_coord
 
-L = 10 #длина стороны куба, в котором находятся частицы
+d = {1.5: 'green', 4.5: 'blue'}
+L = 6 #длина стороны куба, в котором находятся частицы
 N = 2 #число частиц N^3 = 8
 k_B = 1380 #постоянная Больцмана
 T = 300 #температура
-mass = 5 #масса частицы (масса молекулы азота)
+mass = 0.05 #масса частицы (масса молекулы азота)
 av = 1
 #av = m.sqrt(3*k_B*T/mass) #средняя скорость частиц при заданной температуре (из распределения Максвелла)
 Gas=[] #создаем список из элементов класса Particle
 for i in range(N):
     for j in range(N):
         for k in range(N):
-            Gas.append(Particle([float(5*i+2.5),float(5*j+2.5),float(5*k+2.5)],[float(r.gauss(av,av/m.sqrt(3))),float(r.gauss(av,av/m.sqrt(3))),float(r.gauss(av,av/m.sqrt(3)))], mass, L))
+            Gas.append(Particle([float(3*i+1.5),float(3*j+1.5),float(3*k+1.5)],[float(r.gauss(av,av/m.sqrt(3))*r.choice([-1,1])),float(r.gauss(av,av/m.sqrt(3))*r.choice([-1,1])),float(r.gauss(av,av/m.sqrt(3))*r.choice([-1,1]))], mass, L))
 
 for i in range(len(Gas)): #Скорости получаются порядка 500 м/c, что примерно соответствует реальности
     print(Gas[i].velocity)
 
 plt.ion()
-for t in range (700):
+for t in range (250):
     plt.clf()
     for PARTICLES in Gas: #проходим по всем частицам из списка
-        plt.scatter(PARTICLES.coord[0], PARTICLES.coord[1])
-        plt.axis([0, 10, 0, 10])
+        plt.scatter(PARTICLES.coord[0], PARTICLES.coord[1],color = d[PARTICLES.initial_coord[2]])
+        plt.axis([0, 6, 0, 6])
         w = np.zeros(3) #здесь будут складываться ускорения
         #v = PARTICLES.velocity #начальная скорость
         for particle in Gas: #проходим по всем ДРУГИМ частицам из списка
             if PARTICLES != particle:
                 w += PARTICLES.VectorAcceleration(particle) #добавляем ускорение от силы взаимодействия с текущей частицей
         #v += w*0.005 #изменяем скорость
-        PARTICLES.velocity += w*0.05
-        PARTICLES.Move(PARTICLES.velocity*0.025) #двигаем частицу
+        PARTICLES.velocity += 0.01*w
+        PARTICLES.Move(PARTICLES.velocity*0.005) #двигаем частицу
     plt.draw()
-    plt.pause(0.01)
+    plt.pause(0.001)
 plt.ioff()
 plt.show()
 for i in range(len(Gas)):
     print(Gas[i].coord)
+"""
+P = Particle([0,0,0],[0,0,0],5,5)
+Q = Particle([2,0,0],[0,0,0],5,5)
+print(P.Acceleration(Q))
+"""
